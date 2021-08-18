25bb76f8b0caa8d1c35a2478d05164f2ad7bb634
DevStar999 <wolverinerage99@gmail.com>
2021-08-18 07:04:51 +0530

    Selecting a different layout manager for RecyclerView

--- app/src/main/java/com/example/mdrecyclerviewsectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdrecyclerviewsectionproject/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.mdrecyclerviewsectionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -41,8 +41,16 @@ public class MainActivity extends AppCompatActivity {
             add(new User("User 11", "This is the description of User 11", R.drawable.img11));
         }};
 
+        // Here we are setting the Layout Manager to the RecyclerView that of LinearLayoutManager.
+        /*
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
+        */
+
+        // Now, let us set a different Layout Manager to the RecyclerView for e.g. GridLayoutManager.
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(new RecyclerAdapter(sampleUsers, getApplicationContext()));
     }
