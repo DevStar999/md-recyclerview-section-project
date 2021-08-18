e855494aaf6b26024b91115664866dde2ec82182
DevStar999 <wolverinerage99@gmail.com>
2021-08-18 05:52:39 +0530

    Created a POJO for the RecyclerView items called 'User'

---
+++ app/src/main/java/com/example/mdrecyclerviewsectionproject/User.java
@@ -0,0 +1,10 @@
+package com.example.mdrecyclerviewsectionproject;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String userDescription;
+    private int userImage;
+}
